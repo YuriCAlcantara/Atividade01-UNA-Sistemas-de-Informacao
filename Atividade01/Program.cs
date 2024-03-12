a2a253da1dee884ccde3d3e4e19fe104c064e800
YuriCAlcantara <alcantara216@gmail.com>
2024-03-11 21:57:22 -0300

    Primeiros Arquivos

---
+++ Atividade01/Program.cs
@@ -0,0 +1,16 @@
+﻿ using global::System;
+ using global::System.Collections.Generic;
+ using global::System.IO;
+ using global::System.Linq;
+ using global::System.Net.Http;
+ using global::System.Threading;
+ using global::System.Threading.Tasks;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.ReadLine();
+    }
+}
